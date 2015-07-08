f39c966644c40ffc7337f5a94e60e88b72dc041f
Gagandeep Kaur <Gagan@Gagandeeps-MacBook-Pro.local>
2015-07-08 17:38:47 +0530

    Initial Commit

---
+++ CustomTableAPI2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomTableAPI2
+//
+//  Created by Gagandeep Kaur on 08/07/15.
+//  Copyright (c) 2015 Gagandeep Kaur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
